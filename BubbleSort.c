6787df936fe8ebe52fbbefab4c8fa79aecf52f60
BenedictOwler <43406176+BenedictOwler@users.noreply.github.com>
2018-12-11 14:38:28 +0100

    Create BubbleSort.c

---
+++ BubbleSort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+void erzeugeArray(int *, int);
+void zeigeArrayInConsole(int *, int);
+void dreiEcksTausch(int *, int);
+void Sortieren(int *, int);
+
+
+int main()
+{
+int anz = 10;
+int z[anz];
+
+erzeugenArray(z,anz);
+printf("Array mit %d zufaellige Zahlen \n",anz);
+zeigeArrayInConsole(z,anz);
+bubblesort(z,anz);
+printf("\n Array mit %d zufaellige Zahlen sortiert:\n",anz);
+
+zeigeArrayInConsole(z,anz);{
+
+for (int i=0, i < anz, i++){
+
+array[i] = rand();
+
+}
+
+//array = [1,2,3,4,5]
+
+    return 0;
+}
+
+void erzeugeArray(int * array, int anz){
+    
+for (int i=0, i < anz, i++)
+printf("%d",array[i])
+    
+    return 0;
+}
+
+void zeigeArrayInConsole(int * array, int anz){
+    
+    return 0;
+}
+
+void dreiEcksTausch(int * array, int anz){
+    
+    return 0;
+}
+
+void Sortieren(int * array, int anz){
+    
+    return 0;
