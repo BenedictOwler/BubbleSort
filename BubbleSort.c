e68baa43c4b938ff53d27b5d7857dea8b626f325
BenedictOwler <43406176+BenedictOwler@users.noreply.github.com>
2018-12-12 08:11:58 +0100

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,56 +1,72 @@
 #include <stdio.h>
 
-#include <stdio.h>
+void bubblesort (int *, int);
+void erzeugeArray (int *, int);
+void zeigeArrayInConsole (int *, int);
+void dreiEcksTausch (int *, int);
+void Sortieren (int *, int);
 
-void erzeugeArray(int *, int);
-void zeigeArrayInConsole(int *, int);
-void dreiEcksTausch(int *, int);
-void Sortieren(int *, int);
 
-
-int main()
+int
+main ()
 {
-int anz = 10;
-int z[anz];
+  int anz = 10;
+  int z[anz];
 
-erzeugenArray(z,anz);
-printf("Array mit %d zufaellige Zahlen \n",anz);
-zeigeArrayInConsole(z,anz);
-bubblesort(z,anz);
-printf("\n Array mit %d zufaellige Zahlen sortiert:\n",anz);
-
-zeigeArrayInConsole(z,anz);{
-
-srand = 5;
-for (int i=0; i < anz; i++){
-
-array[i] = rand()%100;
+  erzeugeArray (z, anz);
+  printf ("\nArray mit %d zufaellige Zahlen\n", anz);
+  zeigeArrayInConsole (z, anz);
+  bubblesort (z, anz);
+  printf ("Array mit %d zufaellige Zahlen sortiert:\n", anz);
 
 }
+
+zeigeArrayInConsole (int *z, int anz)
+{
+
+  srand (5);
+  for (int i = 0; i < anz; i++)
+    {
+
+      z[i] = rand () % 100;
+
+    }
 
 //array = [1,2,3,4,5]
 
-    return 0;
+  return 0;
 }
 
-void erzeugeArray(int * array, int anz){
-    
-for (int i=0; i < anz; i++)
-printf("%d",array[i])
-    
-    return 0;
+void
+erzeugeArray (int *array, int anz)
+{
+
+  for (int i = 0; i < anz; i++)
+    printf ("%d", array[i]);
+
+  return 0;
 }
 
-void zeigeArrayInConsole(int * array, int anz){
-    
-    return 0;
+
+void
+dreiEcksTausch (int *array, int anz)
+{
+
+  return 0;
 }
 
-void dreiEcksTausch(int * array, int anz){
-    
-    return 0;
+void
+Sortieren (int *array, int anz)
+{
+
+  return 0;
+
 }
 
-void Sortieren(int * array, int anz){
-    
-    return 0;
+void
+bubblesort (int *array, int anz)
+{
+
+  return 0;
+
+}
