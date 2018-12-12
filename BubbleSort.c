7f3321fce97af5132eec85762d320e4ceda9d44d
BenedictOwler <43406176+BenedictOwler@users.noreply.github.com>
2018-12-12 09:30:35 +0100

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -4,7 +4,7 @@ void bubblesort (int *, int);
 void erzeugeArray (int *, int);
 void zeigeArrayInConsole (int *, int);
 void dreiEcksTausch (int *, int);
-void Sortieren (int *, int);
+
 
 
 int
@@ -17,19 +17,20 @@ main ()
   printf ("\nArray mit %d zufaellige Zahlen\n", anz);
   zeigeArrayInConsole (z, anz);
   bubblesort (z, anz);
-  printf ("Array mit %d zufaellige Zahlen sortiert:\n", anz);
+  printf ("\nArray mit %d zufaellige Zahlen sortiert:\n", anz);
+  zeigeArrayInConsole (z, anz);
 
 }
 
-zeigeArrayInConsole (int *z, int anz)
+zeigeArrayInConsole (int *array, int anz)
 {
 
   srand (5);
   for (int i = 0; i < anz; i++)
     {
 
-      z[i] = rand () % 100;
-
+      array[i] = rand () % 100;
+    printf ("%d ", array[i]);
     }
 
 //array = [1,2,3,4,5]
@@ -42,31 +43,44 @@ erzeugeArray (int *array, int anz)
 {
 
   for (int i = 0; i < anz; i++)
-    printf ("%d", array[i]);
+    array[i] = rand()%100;
+    
+    
 
   return 0;
 }
 
 
-void
-dreiEcksTausch (int *array, int anz)
+void dreiEcksTausch (int *array, int position)
 {
-
+    printf("vorher: %d , %d  ",array[position], array[position+1]);
+    int temp = array[position];
+    array[position] = array[position+1];
+    array[position+1] = temp;
+    printf("nacher: %d , %d\n",array[position], array[position+1]);
   return 0;
 }
-
-void
-Sortieren (int *array, int anz)
-{
-
-  return 0;
-
-}
-
-void
-bubblesort (int *array, int anz)
-{
-
-  return 0;
+void bubblesort (int *z, int anz)
+{ int getauscht;
+    for (int d=0; d < anz-1; d++) {
+        
+        getauscht = 0;
+        
+        for (int v=0; v < anz-1-d; v++) {
+            
+            if (z[v] > z[v+1]) {
+                
+                dreiEcksTausch (z,v);
+                getauscht = 1;
+            }
+        }
+        if ( getauscht == 0) {
+            break;
+        }
+        
+    }
+    
+    
+      return 0;
 
 }
